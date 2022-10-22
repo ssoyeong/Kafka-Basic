8900a6c061845e90b0e16451d6f8d5c7021cd0be
ssoyeong <jsy171771@gmail.com>
2022-10-22 20:52:34 +0900

    Initial commit

---
+++ src/main/java/data/mgmt/cdckafka/CdcKafkaApplication.java
@@ -0,0 +1,13 @@
+package data.mgmt.cdckafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/data/mgmt/cdckafka/CdcKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package data.mgmt.cdckafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
